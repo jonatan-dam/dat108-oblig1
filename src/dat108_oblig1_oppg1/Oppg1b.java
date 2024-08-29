6b96623b5e6798653f57d5b47c5ff5e121d78c6d
h678278 <damjonatan@gmail.com>
2024-08-29 12:27:27 +0200

    Oppg 1b

--- src/dat108_oblig1_oppg1/Oppg1b.java
+++ src/dat108_oblig1_oppg1/Oppg1b.java
@@ -1,14 +1,24 @@
 package dat108_oblig1_oppg1;
 
+import java.util.function.BinaryOperator;
+
 public class Oppg1b {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		BinaryOperator<Integer> summering = (Integer a, Integer b) -> a + b;
+		System.out.println("Summen av 12 + 13 = " + beregn(12, 13, summering));
+		
+		BinaryOperator<Integer> storst = (Integer a, Integer b) -> Math.max(a, b);
+		System.out.println("Det største tallet av -5 og 3 er " + beregn(-5, 3, storst));
+		
+		BinaryOperator<Integer> absolutt = (Integer a, Integer b) -> Math.abs((a-b));
+		System.out.println("Absoluttverdien av differansen mellom 54 og 45 er " + beregn(54, 45, absolutt));
 		
 	}
 	
-	public static int beregn(int a, int b) {
-		return 0;
+	public static int beregn(int a, int b, BinaryOperator<Integer> handling) {
+		return handling.apply(a, b);
 	}
 
 }
